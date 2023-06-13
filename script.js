7c5945eb1d9edf41bd87ce40f209c56c95d59af8
dominicjunik <111475701+dominicjunik@users.noreply.github.com>
2023-06-13 14:55:10 -0600

    changed back to original after some testing around

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var menuLinks = [
     {text: 'about', href: '/about'},
     {text: 'catalog', href: '/catalog'},
     {text: 'orders', href: '/orders'},
-    {text: 'account', href: '/account'},
+    {text: 'account', href: '/account'},    
   ];
 
 let mainEl = document.querySelector('main');
